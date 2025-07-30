6e217a49f7fde6d4f2dd244ab752f12b3d20a745
R. Conner Howell <robert@eventualcomputing.com>
2025-07-30 10:10:58 -0700

    test run

--- derezz/index.py
+++ derezz/index.py
@@ -22,15 +22,15 @@ def index(filepath: Path) -> None:
     df = df.with_column("dhash", image_dhash(df["data"]))
     df = df.groupby("dhash").any_value("data", "frame_number")
 
+    # uncomment for production run
+    df = df.limit(5)
+
     # embed images for feature extraction
     df = df.with_column("features", extract_image_features(df["data"])) # type: ignore
 
     # flatten the feature list with its associated frame number
-    df = df.select("frame_number", df["features"].explode())
-    df = df.select("frame_number", df["features"]["*"])
+    df = df.select("frame_number", df["features"].explode()["*"])
+    df = df.filter("label IS NOT NULL")
     df = df.write_parquet("out")
-
-    # 
-
 
     # df.show()
